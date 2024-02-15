26742cd779dd820171d32d908381a7f9138ff21b
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-02-15 17:53:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function attemptLogin() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Perform basic client-side validation
+    if (username === '' || password === '') {
+        alert('Please enter both username and password');
+        return;
+    }
+
+    // Check if the entered credentials match the predefined values
+    if (username === '9415938887' && password === 'heyunix') {
+        alert('Login successful!');
+        
+document.querySelector('.login-container').style.display = 'none';
+                // Show iframe container
+                document.getElementById('iframeContainer').style.display = 'block';
+
+            // Encode the URL in base64
+           var encodedURL = btoa("https://mrp44rth.github.io/GITI-gauriganj-data/");
+
+            // Decode the encoded URL
+           var decodedURL = atob(encodedURL);
+
+            // Set the decoded URL as the src attribute of the iframe
+          document.getElementById('myIframe').src = decodedURL;
+       
+
+ // The new page won't be added to the browser's history
+    } else {
+        alert('Incorrect username or password. Please try again.');
+    }
+}
+
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById('password');
+    var toggleIcon = document.querySelector('.toggle-password');
+
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        toggleIcon.innerHTML = '&#x1F440;';
+    } else {
+        passwordField.type = 'password';
+        toggleIcon.innerHTML = '&#x1F441;';
+    }
+}
