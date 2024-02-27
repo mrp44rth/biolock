3cc2c02c0b458820a32d9f0a6025ac0f0d70de25
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-02-27 20:50:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function attemptLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
@@ -12,46 +11,44 @@ function attemptLogin() {
     // Check if the entered credentials match the predefined values
     if (username === '9415938887' && password === 'p44rth') {
         alert('Login to BioLock is successful!');
-        
-document.querySelector('.login-container').style.display = 'none';
-                // Show iframe container
-                document.getElementById('iframeContainer').style.display = 'block';
 
-            // Encode the URL in base64
-           var encodedURL = btoa("https://mrp44rth.github.io/GITI-gauriganj-data/");
+        document.querySelector('.login-container').style.display = 'none';
+        // Show iframe container
+        document.getElementById('iframeContainer').style.display = 'block';
 
-            // Decode the encoded URL
-           var decodedURL = atob(encodedURL);
+        // Encode the URL in base64
+        var encodedURL = btoa("https://mrp44rth.github.io/GITI-gauriganj-data/");
 
-            // Set the decoded URL as the src attribute of the iframe
-          document.getElementById('myIframe').src = decodedURL;
+        // Decode the encoded URL
+        var decodedURL = atob(encodedURL);
+
+        // Set the decoded URL as the src attribute of the iframe
+        document.getElementById('myIframe').src = decodedURL;
     }
-// second user
-         // Check if the entered credentials match the predefined values
-if (username === '9415938887' && password === 'FAP') {
+    // second user
+    // Check if the entered credentials match the predefined values
+    else if (username === '9415938887' && password === 'FAP') {
         alert('Login to BioLock is successful!');
-        
-document.querySelector('.login-container').style.display = 'none';
-                // Show iframe container
-                document.getElementById('iframeContainer').style.display = 'block';
 
-            // Encode the URL in base64
-           var encodedURL = btoa("https://mrp44rth.github.io/FAP_Data/");
+        document.querySelector('.login-container').style.display = 'none';
+        // Show iframe container
+        document.getElementById('iframeContainer').style.display = 'block';
 
-            // Decode the encoded URL
-           var decodedURL = atob(encodedURL);
+        // Encode the URL in base64
+        var encodedURL = btoa("https://mrp44rth.github.io/FAP_Data/");
 
-            // Set the decoded URL as the src attribute of the iframe
-          document.getElementById('myIframe').src = decodedURL;
-       
+        // Decode the encoded URL
+        var decodedURL = atob(encodedURL);
 
+        // Set the decoded URL as the src attribute of the iframe
+        document.getElementById('myIframe').src = decodedURL;
 
- // The new page won't be added to the browser's history
-    } 
-   else {
+        // The new page won't be added to the browser's history
+    }
+    else {
         alert('Incorrect username or password. Please try again.');
     }
-
+} // This closes the attemptLogin function
 
 function togglePasswordVisibility() {
     var passwordField = document.getElementById('password');
@@ -64,4 +61,4 @@ function togglePasswordVisibility() {
         passwordField.type = 'password';
         toggleIcon.innerHTML = '&#x1F441;';
     }
-}
+} // This closes the togglePasswordVisibility function
